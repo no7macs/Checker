a31fc06655edc7333a9b9718d84adb513f2c9551
no7mac <44577325+no7macs@users.noreply.github.com>
2021-01-12 19:39:46 -0700

    I have to walk now and it sucks, I already hate this and hate it even more now

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import os, sys, zipfile, json
 
+def walk():
+    path = './'
+
+    for root, directories, files in os.walk(path, topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
+        for name in directories:
+            print(os.path.join(root, name))
+
 def main():
-    with open('dat.json','r') as jsondata:
-        loadedjsondata = json.loads(jsondata.read())
-        jsondata.close()
-    
+    # if dat.json exists open it
+    if os.path.isfile('dat.json'):
+        with open('dat.json','r') as jsondata:
+            loadedjsondata = json.loads(jsondata.read())
+            jsondata.close()
+    # if dat.json doesn't exist loop through all zip files for it
+    else: pass
     name = loadedjsondata['zipData']['name'] + '.zip'
 
+    # if zip file already exists check files
     if os.path.isfile(name):
         print('zip exists')
         for a in loadedjsondata['files']:
             zipFile = zipfile.ZipFile(name, 'r')
+            zipFile = zipFile.read(a)
+            # if file doesn't exist create it
             if (not os.path.isfile(a) and not os.path.isdir(a)) and not a in loadedjsondata['exclude']:
                 print('adding removed file')
                 with open(a, 'wb') as newFile:
-                    print(zipFile.read(a))
-                    newFile.write(zipFile.read(a))
+                    print(zipFile)
+                    newFile.write(zipFile)
                     newFile.close()
+            # if file changed revert it
             elif os.path.isfile(a) or os.path.isdir(a):
                 print('Reverting file contents')
                 with open(a, 'rb') as checkFile:
                     checkFileContents = checkFile.read()
                     checkFile.close()
-                if checkFileContents != zipFile.read(a):
+                if checkFileContents != zipFile:
                     with open(a, 'wb') as newVersion:
-                        newVersion.write(zipFile.read(a))
+                        newVersion.write(zipFile)
+    # if zip file doesn't exist create it
     else: 
         loadedjsondata['files'] = []
         newZip = zipfile.ZipFile(name, 'w', zipfile.ZIP_DEFLATED)
@@ -42,4 +59,5 @@ def main():
         main()
 
 if __name__ == "__main__":
-    main()
+    #main()
+    walk()
